b4813c0674b3156b649b97e085032e3c761f75b0
Chetan Sinha <50909804+ChetanSinha@users.noreply.github.com>
2020-03-04 01:39:29 +0530

    Update 8_Queens_Heuristic.py

--- 8 Queens Heuristic/8_Queens_Heuristic.py
+++ 8 Queens Heuristic/8_Queens_Heuristic.py
@@ -1,65 +1,98 @@
-def falsecount(arr, row, column):
+class board():
+	def __init__(self, N):
+		self.arr = [N*[0] for i in range(N)]
+		self.queens=0
+		self.g_cost=0
+		self.h_cost=(N-self.queens)
 
-    count = 0;
+QueueOfBoards = []
+solutions=False
+cost=0
 
-    for i in range(8):
-        if arr[i][column] == 1:
-            count += 1
+def print_(temp):
+	for i in range(len(temp.arr)):
+		for j in range(len(temp.arr[0])):
+			print(temp.arr[i][j], end=" ")
+		print()
 
-    for i,j in zip(range(row-1,-1,-1), range(column-1,-1,-1)):
-        if arr[i][j] == 1:
-            count += 1
+	print("heuristics: ", temp.h_cost)
+	print("path cost: ", temp.g_cost)
+	print("no. of queens: ", temp.queens)
+	print()
 
-    for i,j in zip(range(row+1, 8), range(column+1,8)):
-        if arr[i][j] == 1:
-            count += 1
+# def sorted_by(b1, b2):
+# 	return (b1.h_cost+h1.g_cost)<(b2.h_cost+b2.g_cost) 
 
-    for i,j in zip(range(row-1,-1,-1), range(column+1,8)):
-        if arr[i][j] == 1:
-            count += 1
+def isvalid(temp):
+	q = temp.queens
+	
+	for i in range(8):
+		if temp.arr[q-1][i] == 1:
+			break
+	
+	t = i
 
-    for i,j in zip(range(row+1,8), range(column-1,-1,-1)):
-        if arr[i][j] == 1:
-            count += 1
+	for j in range(q-2, -1, -1):
+		if temp.mat[j][t] == 1:
+			return False
 
-    return count
+	for i,j in zip(range(t-1,-1,-1), range(q-2,-1,-1)):
+		# if j<0 or i<0:
+		# 	break
+		if temp.arr[j][i] == 1:
+			return False
 
+	for i,j in zip(range(t+1,8), range(q-2,-1,-1)):
+		# if j<0 or i>7:
+		# 	break
+		if temp.arr[j][i] == 1:
+			return False
 
-def print_(arr):
-    for i in range(8):
-        for j in range(8):
-            print(arr[i][j], end=" ")
+	
 
-        print()
-
-
-
-def Queen_Hue(arr):
-    row,count,column,count1 = (0,8,0,0)
-
-    for i in range(8):
-        count = 8
-        for j in range(8):
-            arr[i][j] = 0
-            count1 = falsecount(arr, i, j)
-
-            if count > count1:
-                count = count1
-                row = i
-                column = j
-
-        arr[row][column] = 1
-        row = i+1
-        column = 0
-
-    print_(arr)
+	return True
 
 
-if __name__ == "__main__":
+def states(queue):
+	temp = board(8)
+	size = len(queue)
+	
+	global cost
+	global solutions
 
-    arr = [ 8*[0] for i in range(8)]
+	queue.sort(key=lambda x: (x.h_cost + x.g_cost))
 
-    for i in range(8):
-        arr[i][0] = 1
+	temp = queue[0]
+	queue.pop(0)
+	q = temp.queens
 
-    Queen_Hue(arr)
+	if temp.queens == 8:
+		print_(temp)
+		print("Solution found")
+		print("cost: ", cost)
+		solutions=True
+		return
+
+	for i in range(8):
+		temp.arr[q][i] = 1
+		pre = temp.g_cost
+		temp.queens += 1
+		temp.h_cost -= 1
+		temp.g_cost = pre + 1
+
+		if(q==0 or isvalid(temp)):
+			print_(temp)
+			queue.append(temp)
+			cost += 1
+
+		temp.queens -= 1
+		temp.h_cost += 1
+		temp.g_cost = pre
+		temp.arr[q][i] = 0	
+
+
+if __name__ == '__main__':
+	b1 = board(8)
+	QueueOfBoards.append(b1)
+	while(QueueOfBoards and solutions==False):
+		states(QueueOfBoards)
